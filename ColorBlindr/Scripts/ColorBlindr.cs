bf950cbdd6886f057da651f7aa4cc21be2b6a6bc
Thomas Hourdel <chman.a11w@gmail.com>
2014-10-05 19:35:32 +0200

    Initial commit

---
+++ ColorBlindr/Scripts/ColorBlindr.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode, AddComponentMenu("Image Effects/ColorBlindr")]
+[RequireComponent(typeof(Camera))]
+public class ColorBlindr : MonoBehaviour
+{
+	public enum BlindnessType
+	{
+		Normal,
+		Deuteranopia,
+		Protanopia,
+		Tritanopia
+	}
+
+	public BlindnessType Blindness;
+
+	[Range(0.0f, 1.0f)]
+	public float Strength = 1.0f;
+
+	public Shader Shader;
+	protected Material m_Material;
+
+	public Material Material
+	{
+		get
+		{
+			if (m_Material == null)
+			{
+				m_Material = new Material(Shader);
+				m_Material.hideFlags = HideFlags.HideAndDontSave;
+			}
+
+			return m_Material;
+		}
+	}
+
+	void Start()
+	{
+		// Disable if we don't support image effects
+		if (!SystemInfo.supportsImageEffects)
+		{
+			Debug.LogWarning("Image Effects are not supported on this platform.");
+			enabled = false;
+			return;
+		}
+
+		// Disable if we don't support render textures
+		if (!SystemInfo.supportsRenderTextures)
+		{
+			Debug.LogWarning("RenderTextures are not supported on this platform.");
+			enabled = false;
+			return;
+		}
+
+		// Disable the image effect if the shader can't
+		// run on the users graphics card
+		if (Shader != null && !Shader.isSupported)
+		{
+			Debug.LogWarning("Unsupported shader.");
+			enabled = false;
+			return;
+		}
+	}
+
+	void OnDestroy()
+	{
+		if (m_Material)
+			DestroyImmediate(m_Material);
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		if (Shader == null)
+		{
+			Graphics.Blit(source, destination);
+			return;
+		}
+
+		Material.SetFloat("_Strength", Strength);
+		Graphics.Blit(source, destination, Material, (int)Blindness);
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 ColorBlindr
 ===========
 
-Color blindness simulator for Unity3D
+**ColorBlindr** is a color blindness simulator for Unity3D (deuteranopia, protanopia & tritanopia with variable strength). It takes the guesswork out of designing your game for color blindness by showing you in real time what people with common color vision impairments will see. **ColorBlindr** is using the best available algorithms for simulating color vision deficiency, adapted from [Color Oracle](http://colororacle.org/).
+
+You'll find some screenshots [here](http://imgur.com/a/uunaS#0).
+
+Requires Unity Pro 4.3+.
+
+Instructions
+------------
+
+Copy the `ColorBlindr` folder into your project and add the component to your camera (drag it from the `Project` panel or use `Component -> Image Effects -> ColorBlindr`). It should be the very last image effect applied.
+
+Remember to remove or disable it before building the final version of your game.
+
+License
+-------
+
+Zlib (see [LICENSE.txt](LICENSE.txt))
